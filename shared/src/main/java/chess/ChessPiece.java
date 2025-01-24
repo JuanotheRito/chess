a6b753cba767d607f73d3c3d57853e019930da39
JuanotheRito <juanotheunalpo@gmail.com>
2025-01-24 16:54:50 -0700

    implement the rest of the pawn move calculator

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -132,7 +132,60 @@ public class ChessPiece {
         }
         return validMoves;
     }
-
+    /**
+     * Calculates all the valid moves of a black pawn at the position indicated
+     * @param board Current game of chess being played
+     * @param myPosition Position of the pawn
+     * @return ArrayList of all valid moves of the white pawn
+     */
+    public ArrayList<ChessMove> blackPawnMoveCalculator(ChessBoard board, ChessPosition myPosition) {
+        ArrayList<ChessMove> validMoves = new ArrayList<ChessMove>();
+        ChessPosition possibleMove = new ChessPosition(myPosition.getRow(), myPosition.getColumn());
+        boolean hasMoved = false;
+        PieceType[] promotionPieces = new PieceType[4];
+        if (myPosition.getRow() == 2){
+            promotionPieces[0] = PieceType.QUEEN;
+            promotionPieces[1] = PieceType.KNIGHT;
+            promotionPieces[2] = PieceType.ROOK;
+            promotionPieces[3] = PieceType.BISHOP;
+        }
+        if (myPosition.getRow() != 1) {
+            //checks to see if the Pawn has moved
+            if (myPosition.getRow() != 7) {
+                hasMoved = true;
+            }
+            //Checks to see if a double move is valid
+            possibleMove = new ChessPosition((myPosition.getRow()) - 2, myPosition.getColumn());
+            if (!hasMoved && board.getPiece(possibleMove) == null &&
+                    board.getPiece(new ChessPosition(possibleMove.getRow() + 1, possibleMove.getColumn())) == null) {
+                addValidPawnMove(validMoves, myPosition, possibleMove, promotionPieces);
+            }
+            //Checks to see if a single move is valid
+            possibleMove = new ChessPosition((myPosition.getRow()) - 1, myPosition.getColumn());
+            if (board.getPiece(possibleMove) == null) {
+                addValidPawnMove(validMoves, myPosition, possibleMove, promotionPieces);
+            }
+            //checks to see if it can capture left diagonally
+            possibleMove = new ChessPosition((myPosition.getRow()) - 1, (myPosition.getColumn() - 1));
+            if (myPosition.getColumn() != 1) {
+                if (board.getPiece(possibleMove) != null) {
+                    if (board.getPiece(possibleMove).getTeamColor() != board.getPiece(myPosition).getTeamColor()) {
+                        addValidPawnMove(validMoves, myPosition, possibleMove, promotionPieces);
+                    }
+                }
+            }
+            //checks to see if it can capture right diagonally
+            possibleMove = new ChessPosition((myPosition.getRow()) - 1, (myPosition.getColumn()) + 1);
+            if (myPosition.getColumn() != 8) {
+                if (board.getPiece(possibleMove) != null) {
+                    if (board.getPiece(possibleMove).getTeamColor() != board.getPiece(myPosition).getTeamColor()) {
+                        addValidPawnMove(validMoves, myPosition, possibleMove, promotionPieces);
+                    }
+                }
+            }
+        }
+        return validMoves;
+    }
     /**
      * Calculates all the positions a chess piece can move to
      * Does not take into account moves that are illegal due to leaving the king in
@@ -141,9 +194,13 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        if (board.getPiece(myPosition).getPieceType() == PieceType.PAWN
-                && board.getPiece(myPosition).getTeamColor() == ChessGame.TeamColor.WHITE) {
-            return whitePawnMoveCalculator(board, myPosition);
+        if (board.getPiece(myPosition).getPieceType() == PieceType.PAWN){
+            if(board.getPiece(myPosition).getTeamColor() == ChessGame.TeamColor.WHITE) {
+                return whitePawnMoveCalculator(board, myPosition);
+            }
+            else{
+                return blackPawnMoveCalculator(board, myPosition);
+            }
         }
         throw new RuntimeException("Not implemented");
     }
