74723fee49371e769f3bc90d92e4feacb9c2e1f2
JuanotheRito <juanotheunalpo@gmail.com>
2025-03-03 20:46:06 -0700

    Finish writing tests for listGame endpoint and begin writing service code

--- server/src/test/java/service/GameServiceTests.java
+++ server/src/test/java/service/GameServiceTests.java
@@ -1,7 +1,12 @@
 package service;
 
+import chess.ChessGame;
 import dataaccess.DataAccessException;
+import dataaccess.GameDAO;
+import model.GameData;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -35,7 +40,7 @@ class GameServiceTests {
         ClearService.clear();
     }
     @Test
-    void invalidAuthorization(){
+    void invalidCreateAuthorization(){
         try {
             UserService.register(new RegisterRequest("CosmoCougar", "GoCougars!", "cosmo@byu.edu"));
         }
@@ -52,6 +57,43 @@ class GameServiceTests {
         }
         CreateRequest test = new CreateRequest("1", "GG");
         assertThrows(DataAccessException.class, () -> GameService.createGame(test));
+        ClearService.clear();
+    }
+
+    void listGamesSuccessfully(){
+        try {
+            UserService.register(new RegisterRequest("CosmoCougar", "GoCougars!", "cosmo@byu.edu"));
+        }
+        catch(Exception e){
+            throw new RuntimeException(e);
+        }
+        LoginResult loginResult = null;
+        LoginRequest login = new LoginRequest("CosmoCougar", "GoCougars!");
+        try {
+            loginResult = UserService.login(login);
+        }
+        catch (Exception e){
+            throw new RuntimeException(e);
+        }
+        CreateRequest create = new CreateRequest(loginResult.authToken(), "GG");
+        try {
+            GameService.createGame(create);
+        }
+        catch (Exception e){
+            throw new RuntimeException(e);
+        }
+        GameListRequest test = new GameListRequest(loginResult.authToken());
+        ArrayList<GameData> expectedList = new ArrayList<>();
+        expectedList.add(new GameData(1, null, null, "GG", new ChessGame()));
+        GameListResult expected = new GameListResult(expectedList);
+        GameListResult actual = null;
+        try{
+            actual = GameService.listGames(test);
+        }
+        catch (Exception e){
+            throw new RuntimeException(e);
+        }
+        assertEquals(expected, actual);
         ClearService.clear();
     }
 }
