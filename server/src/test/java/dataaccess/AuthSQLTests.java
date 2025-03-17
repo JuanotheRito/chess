ee96aa98b27963ba96e8bdb3218ec79860e6d063
JuanotheRito <juanotheunalpo@gmail.com>
2025-03-17 13:30:48 -0600

    rename test directories

--- server/src/test/java/dataaccess/AuthSQLTests.java
+++ server/src/test/java/dataaccess/AuthSQLTests.java
@@ -1,4 +1,4 @@
-package service;
+package dataaccess;
 
 import dataaccess.AuthDAO;
 import dataaccess.DataAccessException;
--- server/src/test/java/dataaccess/UserSQLTests.java
+++ server/src/test/java/dataaccess/UserSQLTests.java
@@ -1,4 +1,4 @@
-package service;
+package dataaccess;
 
 import dataaccess.*;
 import model.AuthData;
